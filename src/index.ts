884922648fb976afb4573c4e1a2d790524b859e9
syahrulapra <syahrulap27@gmail.com>
2024-01-19 11:00:54 +0700

    Fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ const app = new Hono()
 
 dotenv.config()
 
-const DB_URL = 'mongodb://0.0.0.0:27017/ticketing'
-
-mongoose.connect(DB_URL)
+mongoose.connect(process.env.DB_URL!)
 const conn = mongoose.connection
 
 conn.on('error', () => {
